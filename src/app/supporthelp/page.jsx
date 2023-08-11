8629f5d04464cac3a42a064c9308a6627d87884b
argenestel <arpitpali68@gmail.com>
2023-08-11 23:23:47 +0530

    help

--- src/app/supporthelp/page.jsx
+++ src/app/supporthelp/page.jsx
@@ -5,9 +5,8 @@ import Image from 'next/image'
 export default function SupportHelp() {
   return (
    <div>
-    <h1>
-    How to Delete Data and Account
-    </h1>
+            <h1 className="text-3xl font-bold mb-2">How to Delete Data and Remove Account</h1>
+
     <Image
       src="/requestaccount.png"
       width={1920}
