63db87edd407538d6def79c56fdc553ddfbf90da
argenestel <arpitpali68@gmail.com>
2023-08-16 20:19:50 +0530

    new policies and terms

--- src/app/privacy/page.jsx
+++ src/app/privacy/page.jsx
@@ -12,7 +12,7 @@ export default function Privacy() {
             <h1 className="text-3xl font-bold mb-2">Privacy Policy for Sikka2x</h1>
             <h2 className="text-xl font-bold mb-1">1. Information Collection:</h2>
             <p className="mb-2"> 
-When you use Sikka2x, we may collect various types of information to provide you with a seamless and personalized experience. This includes essential personal details like your name, email address, contact number, and payment information, which are necessary for user registration and verification processes.
+            When you use Sikka2x, we may collect various types of information to provide you with a seamless and personalized experience. This includes essential personal details like your name, email address, contact number, and payment information, which are necessary for user registration and verification processes.
 Additionally, we may gather non-personal information such as your device type, IP address, and usage patterns. These insights help us understand how users interact with the app, enabling us to make continuous improvements and optimize our services.
 </p>
 
@@ -28,7 +28,7 @@ Our dedicated team continuously monitors and updates our systems, ensuring they 
 </p>
 
             <h2 className="text-xl font-bold mb-1">4. Third-party Sharing:</h2>
-            <p className="mb-2">In certain instances, to enhance the functionality and user experience of Sikka2x, we may collaborate with trusted third-party service providers. These collaborations may involve sharing certain user information.
+            <p className="mb-2">n certain instances, to enhance the functionality and user experience of Sikka2x, we may collaborate with trusted third-party service providers. These collaborations may involve sharing certain user information.
 However, we want to assure you that our partnerships are bound by strict confidentiality agreements. These third parties are not permitted to use your data for any purposes other than those directly related to improving the app's performance, facilitating payment processing, or conducting necessary analytics.
 </p>
 
@@ -47,12 +47,49 @@ You have the freedom to manage your cookie preferences through your browser sett
 </p>
 
             <h2 className="text-xl font-bold mb-1">8. Opt-out:</h2>
-            <p className="mb-2">If, at any point, you wish to stop receiving promotional communications from Sikka2x, you have the option to opt-out. Simply follow the instructions provided in the emails to unsubscribe from such communications.
+            <p className="mb-2">If, at any point, you wish to stop receiving promotional communications from Sikka2x, you have the option to opt-out. Simply follow the instructions provided here - https://sikka2x.com/supporthelp to unsubscribe from such communications.
+</p>
+<h2 className="text-xl font-bold mb-1">9. Grievance procedure: </h2>
+            <p className="mb-2">
+            <p className="mb-4">
+        At Sikka2x, we value our players and strive to provide a fair and enjoyable gaming experience. We understand that concerns and grievances may arise from time to time, and we are committed to addressing them promptly and fairly. This Grievance Procedure outlines the steps you can take if you have a grievance related to the Sikka2x real money gaming application.
+      </p>
+      <div className="pl-4">
+        <p className="mb-2">
+          Contact Customer Support: If you have a grievance or concern, please contact our Customer Support team. You can reach them through the following channels:
+        </p>
+        <ul className="list-disc list-inside mb-4">
+          <li>
+            In-app Help Center: Navigate to the "Profile" or "Support" section within the Sikka2x application. You can submit a support ticket or live chat with a customer support representative.
+          </li>
+          <li>
+            Email: Send an email to <a href="mailto:help@sikka2x.com" className="text-blue-500 underline">help@sikka2x.com</a> with a detailed description of your grievance.
+          </li>
+        </ul>
+        <p className="mb-2">
+          When contacting Customer Support, please provide the following information:
+        </p>
+        <ol className="list-decimal list-inside mb-4">
+          <li>Your username</li>
+          <li>A clear and concise description of the grievance</li>
+          <li>Any relevant screenshots or evidence</li>
+        </ol>
+        <p className="mb-2">
+          Our Customer Support team will investigate your grievance and provide you with a response within a reasonable timeframe.
+        </p>
+      </div>
 </p>
 
-            <h2 className="text-xl font-bold mb-1">9. Changes to Policy:</h2>
-            <p className="mb-2">Sikka2x reserves the right to update and modify this Privacy Policy...</p>
-            <p>This Privacy Policy was last updated on 23 July 2023.</p>
+            <h2 className="text-xl font-bold mb-1">10. Changes to Policy:</h2>
+            <p className="mb-2">Sikka2x reserves the right to update and modify this Privacy Policy as necessary to adapt to changing circumstances or to enhance our data protection practices. Any revisions will be promptly communicated to users through the app or other appropriate channels.
+ 
+By using Sikka2x, you provide your consent to the practices outlined in this Privacy Policy. We encourage you to read and understand this policy to make informed decisions about your data. If you have any questions or concerns regarding your data or this policy, please feel free to contact us at help@sikka2x.com. 
+</p>
+
+<br />
+
+This Privacy Policy was last updated on 23 July 2023.
+
         </div>
     </div>
 </div>
--- src/app/terms/page.jsx
+++ src/app/terms/page.jsx
@@ -9,89 +9,89 @@ export default function Terms() {
 </Head>
 <div className="min-h-screen flex flex-col bg-amber-100 p-10 ">
 <div className="container mx-auto px-4">
-            <h1 className="text-3xl font-bold mb-2">Terms and Condition for Sikka2x</h1>
+            <h1 className="text-3xl font-bold mb-3">Terms and Condition for Sikka2x</h1>
          <div>
+         Before accessing Sikka2x, it is essential to review and agree to the Terms and Conditions. These terms outline the rights, responsibilities, and limitations associated with using the game. By continuing to play Sikka2x, you acknowledge and accept the terms specified.
 
-Before accessing Sikka2x, it is essential to review and agree to the Terms and Conditions. These terms outline the rights, responsibilities, and limitations associated with using the game. By continuing to play Sikka2x, you acknowledge and accept the terms specified.
+         <h2 className="text-xl font-bold mb-2 mt-4">Eligibility:</h2> To play the Game, you must be at least 18 years of age. You represent and warrant that you have the right, authority, and capacity to enter into these Terms and to abide by all of the terms and conditions set forth herein. The Game is not intended for and is not to be used by individuals under the age of 18.
 
-Eligibility: To play the Game, you must be at least 18 years of age. You represent and warrant that you have the right, authority, and capacity to enter into these Terms and to abide by all of the terms and conditions set forth herein. The Game is not intended for and is not to be used by individuals under the age of 18.
-<br />
-<br />
-Technical Requirements:  As a user of Sikka2x, you are responsible for maintaining an up-to-date Android version (Level 33/Android 13) on your device and ensuring a stable internet connection during gameplay. Please understand that any loss or disruption caused by inadequate device requirements or internet connectivity issues will not be the responsibility of Sikka2x.
-<br />
-<br />
-a.	Minimum Android Version Requirement: To ensure a smooth and optimal experience while using Sikka2x, it is mandatory that your Android device is running on at least Android Level 33 (Android 13). Older versions may not be compatible with the app's features and functionalities, and users may encounter performance issues.
-<br />
-<br />
-b.	Stable Internet Connection: Sikka2x relies on a stable internet connection for seamless gameplay and real-time interactions with opponents. Before participating in a cricket quiz, ensure that your internet connection is stable to avoid any interruptions during the game. When you are successfully mapped to an opponent, 5 rupees will be auto-deducted from your wallet to cover the entry fee for the quiz. If your internet connection is lost at this crucial point, you risk losing the entry fee amount of 5 rupees.
-<br />
-<br />
-c.	Considerations for Opponent Mapping: Sikka2x automatically matches players with random opponents for a competitive cricket quiz experience. It is important to have a stable internet connection during this mapping process to avoid any discrepancies or disruptions in the game.
-<br />
-<br />
-d.	Loss of Internet during Quiz Participation: In the event of a sudden loss of internet connection while you are actively participating in a cricket quiz, there is a possibility that you may lose the quiz round. Losing the round would mean losing the 5 rupees deducted from your wallet for the quiz entry.
-To prevent such situations, we strongly advise ensuring a stable internet connection throughout the entire duration of the quiz.
-<br />
-<br />
-Risk Acknowledgment: The Game involves risk because it requires the use of real money. By participating, you acknowledge and accept the risks associated with such participation, including the potential loss of all monies deposited into your account.
-<br />
-<br />
-User Accounts: In order to access the Game, you need to create an account. When creating an account, you must provide accurate and complete information. You are solely responsible for the activity that occurs on your account, and you must keep your account password secure.
-<br />
-<br />
-Gameplay: The Game is a quiz-based game of skill. You acknowledge that the outcome of each Game is determined based on your responses to quiz questions that are generated randomly. Your winnings will depend on your skills and performance in the Game.
-<br />
-<br />
-Payments and Financial Transactions: All financial transactions made within the Game are processed using third-party payment processors. You acknowledge that Sikka2X is not responsible for any losses or damages incurred as a result of any third-party involvement in these transactions.
-<br />
-<br />
-Withdrawal of Winnings: You may withdraw your winnings subject to these Terms, and any other rules that we may impose from time to time. Such rules may include verification checks, minimum withdrawal amounts, and processing fees. For first withdrawal the player must completed the KYC, post that only he will be able to withdraw his earnings.
-<br />
-<br />
-Responsible Gaming: We provide you with features that allow you to manage and limit your gaming experience to ensure responsible gaming. You must use these tools as necessary to maintain control of your gaming activities.
-<br />
-<br />
-Intellectual Property: The Game and its original content, features, and functionality are and will remain the exclusive property of Sikka2X. These elements are protected by copyright, trademark, and other laws. Our trademarks and trade dress may not be used in connection with any product or service without the prior written consent of Sikka2X.
-<br />
-<br />
-Disclaimer of Liability:
+         <h2 className="text-xl font-semibold mb-4 mt-4">Technical Requirements</h2>
+      <p className="mb-4">
+        As a user of Sikka2x, you are responsible for maintaining an up-to-date Android version (Level 33/Android 13) on your device and ensuring a stable internet connection during gameplay. Please understand that any loss or disruption caused by inadequate device requirements or internet connectivity issues will not be the responsibility of Sikka2x.
+      </p>
+      <div className="pl-4">
+        <p className="mb-2">
+          <p className="font-semibold"> a. Minimum Android Version Requirement:</p> To ensure a smooth and optimal experience while using Sikka2x, it is mandatory that your Android device is running on at least Android Level 33 (Android 13). Older versions may not be compatible with the app's features and functionalities, and users may encounter performance issues.
+        </p>
+        <p className="mb-2">
+        <p className="font-semibold">b. Stable Internet Connection:</p> Sikka2x relies on a stable internet connection for seamless gameplay and real-time interactions with opponents. Before participating in a cricket quiz, ensure that your internet connection is stable to avoid any interruptions during the game. When you are successfully mapped to an opponent, 5 rupees will be auto-deducted from your wallet to cover the entry fee for the quiz. If your internet connection is lost at this crucial point, you risk losing the entry fee amount of 5 rupees.
+        </p>
+        <p className="mb-2">
+        <p className="font-semibold">c. Considerations for Opponent Mapping:</p> Sikka2x automatically matches players with random opponents for a competitive cricket quiz experience. It is important to have a stable internet connection during this mapping process to avoid any discrepancies or disruptions in the game.
+        </p>
+        <p className="mb-2">
+        <p className="font-semibold">d. Loss of Internet during Quiz Participation:</p> In the event of a sudden loss of internet connection while you are actively participating in a cricket quiz, there is a possibility that you may lose the quiz round. Losing the round would mean losing the 5 rupees deducted from your wallet for the quiz entry.
+          To prevent such situations, we strongly advise ensuring a stable internet connection throughout the entire duration of the quiz.
+        </p>
+      </div>
+<h2 className="text-xl font-bold mb-1 mt-4">Risk Acknowledgment:</h2> The Game involves risk because it requires the use of real money. By participating, you acknowledge and accept the risks associated with such participation, including the potential loss of all monies deposited into your account.
+
+<h2 className="text-xl font-bold mb-1 mt-4">User Accounts:</h2> In order to access the Game, you need to create an account. When creating an account, you must provide accurate and complete information. You are solely responsible for the activity that occurs on your account, and you must keep your account password secure.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Gameplay:</h2> The Game is a quiz-based game of skill. You acknowledge that the outcome of each Game is determined based on your responses to quiz questions that are generated randomly. Your winnings will depend on your skills and performance in the Game.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Payments and Financial Transactions:</h2> All financial transactions made within the Game are processed using third-party payment processors. You acknowledge that Sikka2X is not responsible for any losses or damages incurred as a result of any third-party involvement in these transactions.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Withdrawal of Winnings:</h2> You may withdraw your winnings subject to these Terms, and any other rules that we may impose from time to time. Such rules may include verification checks, minimum withdrawal amounts, and processing fees. For first withdrawal the player must completed the KYC, post that only he will be able to withdraw his earnings.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Responsible Gaming:</h2> We provide you with features that allow you to manage and limit your gaming experience to ensure responsible gaming. You must use these tools as necessary to maintain control of your gaming activities.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Intellectual Property:</h2> The Game and its original content, features, and functionality are and will remain the exclusive property of Sikka2X. These elements are protected by copyright, trademark, and other laws. Our trademarks and trade dress may not be used in connection with any product or service without the prior written consent of Sikka2X.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Disclaimer of Liability:</h2>
 While Sikka2x endeavours to provide a seamless and enjoyable gaming experience, we cannot guarantee uninterrupted access or the absence of technical issues. We disclaim any liability for any loss, damage, or inconvenience caused by factors beyond our control, including but not limited to network connectivity, device compatibility, or third-party service interruptions.
-<br />
-<br />
+
+<h2 className="text-xl font-bold mb-1 mt-4">TDS (Tax Deducted at Source) Policy on winnings: </h2>
+<p className="mb-4">
+        1. Sikka2x will deduct TDS on winnings and earnings at the prevailing rates as per the Income Tax laws of the Government of India.
+      </p>
+      <p className="mb-4">
+        2. The TDS will be deducted at the time of crediting winnings to the Player's account or at the time of actual payment, whichever is earlier.
+      </p>
+      <p className="mb-4">
+        3. A TDS certificate will be issued to the Player in compliance with applicable tax laws, reflecting the details of the TDS deducted and deposited with the tax authorities. This Certificate will not be issued right after the payment as we are building the system in our application.
+      </p>
+      <p className="mb-4">
+        4. The TDS rate for winnings and earnings from the Sikka2x Platform shall be in accordance with the prevailing tax laws. Currently, the TDS rate for gaming winnings is charged at a flat rate of 30%.
+      </p>
+      <p className="mb-4">
+        5. This TDS Policy is subject to change in accordance with updates to tax laws and regulations. Sikka2x reserves the right to modify, amend, or update this Policy as per the new rules and laws. Players can check any changes to the Policy through the mobile application "Sikka2x" or website <a href="https://sikka2x.com/" className="text-blue-500 underline">https://sikka2x.com/</a>.
+      </p>
+<h2 className="text-xl font-bold mb-1 mt-4">Restricted States:</h2>
+ As Sikka2x allows users to play with real money, It is prohibited in Assam, Odisha, Nagaland, Sikkim, Aandra Pradesh and Telangana. Users in these states must comply with local laws. Sikka2x reserves the right to suspend or terminate accounts found in violation. Users are responsible for adhering to state regulations and Sikka2x assumes no liability. Stay informed about changing laws. By using Sikka2x, users accept these restrictions. For legal advice, consult professionals or official sources.
 Privacy and Data Protection:
 Sikka2x prioritizes the privacy and protection of players' personal information. We adhere to strict data protection policies and comply with applicable privacy laws. Your personal data, including name, email, and gameplay statistics, will be handled securely and used solely for the purpose of enhancing your gaming experience and providing relevant updates.
-<br />
-<br />
-Conduct: You agree to abide by all local, state, national, and international laws and regulations applicable to your use of the Game, and you agree not to interfere with the use and enjoyment of the Game by other users.
-<br />
-<br />
-Modification of Rules:
+
+<h2 className="text-xl font-bold mb-1 mt-4">Conduct:</h2> You agree to abide by all local, state, national, and international laws and regulations applicable to your use of the Game, and you agree not to interfere with the use and enjoyment of the Game by other users.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Modification of Rules:</h2>
 Sikka2x reserves the right to modify or update the game rules and regulations as deemed necessary. Players will be notified of any significant changes through in-game announcements or other communication channels. It is the players' responsibility to stay informed about the latest rules and abide by them.
-<br />
-<br />
-Account Suspension and Termination:
+
+<h2 className="text-xl font-bold mb-1 mt-4">Account Suspension and Termination:</h2>
 Sikka2x reserves the right to suspend or terminate accounts that violate the game's rules and regulations. This includes but is not limited to cheating, hacking, exploiting loopholes, or engaging in any form of misconduct. Players found to be in breach of these rules may face temporary or permanent suspension, depending on the severity of the offense.
-<br />
-<br />
-Governing Law: These Terms, and any disputes arising out of or related to them or the Game, will be governed in accordance with the laws of the jurisdiction in which the company is registered, without regard to its conflict of law principles.
-<br />
-<br />
-Dispute Resolution: In the event of a dispute, please email our customer service team at help@sikka2x.com. If we cannot resolve the dispute through communication, then the dispute will be resolved in accordance with the laws of the jurisdiction in which the company is registered.
-<br />
-<br />
-Changes to the Terms: Sikka2X reserves the right, at our discretion, to change, modify, add, or remove portions of these Terms at any time. If the Terms are changed, we will notify you by posting the updated Terms on the Game or through other means.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Governing Law: </h2>These Terms, and any disputes arising out of or related to them or the Game, will be governed in accordance with the laws of the jurisdiction in which the company is registered, without regard to its conflict of law principles.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Dispute Resolution: </h2>In the event of a dispute, please email our customer service team at help@sikka2x.com. If we cannot resolve the dispute through communication, then the dispute will be resolved in accordance with the laws of the jurisdiction in which the company is registered.
+
+<h2 className="text-xl font-bold mb-1 mt-4">Changes to the Terms:</h2> Sikka2X reserves the right, at our discretion, to change, modify, add, or remove portions of these Terms at any time. If the Terms are changed, we will notify you by posting the updated Terms on the Game or through other means.
 We hope these terms and conditions provide a clear understanding of the guidelines and expectations for playing Sikka2x. By participating in the game, you acknowledge and agree to abide by these terms and conditions. Any future amendments and additions to these Terms as published from time to time.
 Enjoy the excitement, challenge, and rewards of Sikka2x as you engage in thrilling quiz battles and compete for victory. Have a great gaming experience!
+
+
 <br />
 <br />
-
-Last Updated: July 23, 2023
-
-
-
-
-
-
+Last Updated: August 16, 2023
 
 
          </div>
