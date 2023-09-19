5cb43af306f678415cace05d235e6a669714a2e4
Argenestel <49126445+argenestel@users.noreply.github.com>
2023-09-19 23:49:59 +0530

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -155,10 +155,40 @@ export default function Section({ type, title })
                     </div>
                 </div>
             )}
-            { type === 'contact' && (
-
+            {type === 'contact' && (
                 <div id="contact" className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-                    
+                    {/* <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                        <iframe
+                            width="100%"
+                            height="100%"
+                            title="map"
+                            className="absolute inset-0"
+                            style={{ filter: "opacity(0.7)" }}
+                            src="https://www.google.com/maps/embed?pb=..."
+                        />
+                        <div className="bg-primaryRed relative flex flex-wrap py-6 rounded shadow-md text-primaryYellow">
+                            <div className="lg:w-1/2 px-6">
+                                <h2 className="title-font font-semibold text-primaryYellow tracking-widest text-xs">
+                                    ADDRESS
+                                </h2>
+                                <p className="mt-1">
+                                    123 Betting St, City, Country
+                                </p>
+                            </div>
+                            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                                <h2 className="title-font font-semibold text-primaryYellow tracking-widest text-xs">
+                                    EMAIL
+                                </h2>
+                                <a className="leading-relaxed text-primaryYellow">
+                                    support@bettingapp.com
+                                </a>
+                                <h2 className="title-font font-semibold text-primaryYellow tracking-widest text-xs mt-4">
+                                    PHONE
+                                </h2>
+                                <p className="leading-relaxed text-primaryYellow">+123 456 7890</p>
+                            </div>
+                        </div>
+                    </div> */}
                     <form
                         name="contact"
                         className="flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0 bg-primaryRed rounded-lg p-6">
@@ -206,7 +236,7 @@ export default function Section({ type, title })
                         </button>
                     </form>
                 </div>
-            )};
+            )}
         </section>
     );
 };
