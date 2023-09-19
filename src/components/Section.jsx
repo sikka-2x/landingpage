ecd67146aaea0c828d0e1bd91cc9cfc862f0bddc
Argenestel <49126445+argenestel@users.noreply.github.com>
2023-09-19 23:51:04 +0530

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -21,7 +21,7 @@ export default function Section({ type, title })
                     </div>
                 </div>
             
-                )};
+                )}
 
             {type === 'how-it-works' && (
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4 bg-amber-100 text-primaryRed `rounded-lg" id="about">
